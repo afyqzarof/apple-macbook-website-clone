1423aef28c54584d094d67739a383f23fda480b2
afyqeyzar <norafyqe@gmail.com>
2024-06-19 21:02:39 +0800

    feat: homepage header for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,17 @@
 import appleLogo from "../../public/apple-logo.svg";
+import searchIcon from "../assets/icons/search.svg";
+import menuIcon from "../assets/icons/menu.svg";
+import basketIcon from "../assets/icons/basket.svg";
 
 const Header = () => {
   return (
-    <header>
-      <img src={appleLogo} alt="apple logo" className="w-16"/>
-      <ul>
-        <li>
-          <a>Store</a>
-        </li>
-      </ul>
+    <header className="flex h-10 justify-between p-2 items-center">
+      <img src={appleLogo} alt="apple logo" className="w-5" />
+      <section className="flex items-center justify-between gap-6">
+        <img src={searchIcon} alt="search" className="w-5" />
+        <img src={basketIcon} alt="basket" className="w-5" />
+        <img src={menuIcon} alt="menu" className="w-5" />
+      </section>
     </header>
   );
 };
